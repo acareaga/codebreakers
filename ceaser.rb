e3f640258308d6830c4a0fdbc27bda0f3ed3c78a
acareaga <aaroncareaga@gmail.com>
2016-02-12 08:22:41 -0700

    Added changes from Chelsea and Sunny for demo

--- ceaser.rb
+++ ceaser.rb
@@ -1,22 +1,33 @@
 require 'pry'
 
-class Ceaser
-  attr_reader :message, :rotation, :character_map
+class Caesar
+  attr_reader :message, :rotation, :character_map, :words
+
   def initialize(rotation, message)
     @rotation = rotation
-    @message = message.downcase.chars
-    @character_map = ('a'..'z').to_a + [" ", ".", ",", "!"]
+    @words = message.split(" ")
+    @character_map = ('a'..'z').to_a
   end
 
   def wrap
-    message.map { |letter| index = character_map.index(letter)
-                       new_index = (index + rotation) % 30
-                       character_map[new_index]
-                }.join
+    words.map{|word| word.chars.map{|letter| encrypt_character(letter)}.join("")}.join(" ")
+  end
+
+  def encrypt_character(letter)
+      if character_map.include?(letter.downcase)
+        is_lower = letter.downcase == letter
+        index = character_map.index(letter.downcase)
+        new_index = (index + rotation) % 26
+        new_letter = character_map[new_index]
+        is_lower ? new_letter.downcase : new_letter.upcase #check case
+      else
+        letter
+      end
   end
 end
 
+
 if __FILE__ == $PROGRAM_NAME
-  ceaser = Ceaser.new(13, "Be sure to drink you Ovaltine!")
-  puts ceaser.wrap
+  caesar = Caesar.new(13, "Be sure to drink you Ovaltine!")
+  puts caesar.wrap
 end
